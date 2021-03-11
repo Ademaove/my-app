6be128646c7211a0f9fecf568b5c795214d040bc
Ademaove <58460164+Ademaove@users.noreply.github.com>
2021-03-11 15:46:51 +0600

    Simple login page
    First experience with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import LoginForm from './componeents/LoginForm';
+
 import './App.css';
 
 function App() {
+  const admin={
+    email: "test@test.com",
+    password: "12345"
+  }
+  const[user, setUser]=useState({name: "", email: "", password: ""});
+  const[error, setError]=useState("");
+  const Login=details  => {
+    console.log(details);
+    if(details.email==admin.email && details.password==admin.password){
+      console.log("Success!");
+      setUser({
+        name: details.name,
+        email: details.email
+      })
+    }else{
+      console.log("Error");
+      setError("Error!");
+    }
+  }
+
+  const Logout= ()  => {
+    console.log("You logged out");
+    setUser({name: "", email: ""});
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {(user.email!="") ?(
+        <div className="welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+          </div>
+      ): (
+        <LoginForm Login={Login} error={error}/>
+      )}
     </div>
   );
+  
 }
 
 export default App;
